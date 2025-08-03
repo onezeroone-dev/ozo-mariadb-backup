815a3b05b84a9bcbffaa2a7cb609c5e4e4bd97d5
Andy Lievertz <alievertz@onezeroone.dev>
2025-08-02 18:25:34 -0600

    All features implented; v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# OZO MariaDB Backup Change Log
+
+|Date|Version|Comment|
+|----|-------|-------|
+|2023-Mar-17|1.0.0|Initial release.|
--- ozo-mariadb-backup.sh
+++ ozo-mariadb-backup.sh
@@ -1,170 +1,208 @@
 #!/bin/bash
+# Script Name: ozo-mariadb-backup.sh
+# Version    : 1.0.0
+# Description: Creates a dump of all MariaDB databases and performs history maintenance.
+# Usage      : /usr/sbin/ozo-mariadb-backup.sh
+# Author     : Andy Lievertz <alievertz@onezeroone.dev>
+# Link       :
 
 # FUNCTIONS
-
 function ozo-log {
-  ### Logs output to the system log
-  if [[ -z "${LEVEL}" ]]
-  then
-    LEVEL="info"
-  fi
-  if [[ -n "${MESSAGE}" ]]
-  then
-    logger -p local0.${LEVEL} -t "OZO MariaDB Backup" "${MESSAGE}"
-  fi
+    # Function   : ozo-log
+    # Description: Logs output to the system log
+    # Arguments  :
+    #   LEVEL    : The log level. Allowed values are "err", "info", or "warning". Defaults to "info".
+    #   MESSAGE  : The message to log.
+    
+    # Determine if LEVEL is null
+    if [[ -z "${LEVEL}" ]]
+    then
+        # Level is null; set to "info"
+        LEVEL="info"
+    fi
+    # Determine if MESSAGE is not null
+    if [[ -n "${MESSAGE}" ]]
+    then
+        # Message is not null; log the MESSAGE with LEVEL
+        logger -p local0.${LEVEL} -t "OZO MariaDB Backup" "${MESSAGE}"
+    fi
 }
 
 function ozo-mariadb-dump-create-output-dir {
-  ### Checks for the existence of the output directory and creates it if missing
-  ### Returns 0 (TRUE) if the directory exists or is created successfully and 1 (FALSE) if the directory does not exist and cannot be created
-  local RETURN=0
-  if [[ -d "${MARIADB_DUMP_DIR}" ]]
-  then
-    LEVEL="info" MESSAGE="Found output directory ${MARIADB_DUMP_DIR}" ozo-log
-  else
-    if mkdir -p "${MARIADB_DUMP_DIR}"
+    # Function   : ozo-mariadb-dump-create-output-dir
+    # Description: Checks for the existence of the output directory and creates it if missing. Returns 0 (TRUE) if the directory exists or is created successfully and 1 (FALSE) if the directory does not exist and cannot be created.
+
+    # Control variable
+    local RETURN=0
+    # Determine if the dump directory does not exist
+    if [[ ! -d "${MARIADB_DUMP_DIR}" ]]
     then
-      LEVEL="info" MESSAGE="Created output directory ${MARIADB_DUMP_DIR}" ozo-log
-    else
-      LEVEL="err" MESSAGE="Unable to create output directory ${MARIADB_DUMP_DIR}" ozo-log
-      RETURN=1
+        # Directory does not exist; attempt to create
+        if mkdir -p "${MARIADB_DUMP_DIR}"
+        then
+            # Success
+        else
+            # Failure
+            RETURN=1
+        fi
     fi
-  fi
-  return ${RETURN}
+    # Return
+    return ${RETURN}
 }
 
 function ozo-mariadb-dump-validate-configuration {
-  ### Performs a series of checks against the script configuration
-  ### Returns 0 (TRUE) if all checks pass and 1 (FALSE) if any check fails
-  local RETURN=0
-  MARIADB_DUMP_CONFIGURATION="/etc/ozo-mariadb-backup.conf"
-  if [[ -f "${MARIADB_DUMP_CONFIGURATION}" ]]
-  then
-    # source the configuration
-    source "${MARIADB_DUMP_CONFIGURATION}"
-    # check that all user-defined variables are set
-    for USERDEFVAR in MARIADB_DUMP_USER MARIADB_DUMP_PASS MARIADB_DUMP_DIR MARIADB_DUMP_SKIP_DB
-    do
-      if [[ -z "${!USERDEFVAR}" ]]
-      then
-        LEVEL="err" MESSAGE="User-defined variable ${USERDEFVAR} is not set." ozo-log
-        RETURN=1
-      fi
-    done
-    # check that all required binaries are found in the path
-    for BINARY in mysql mysqldump mkdir chown find bzip2 rm
-    do
-      if ! which ${BINARY}
-      then
-        LEVEL="err" MESSAGE="Missing ${BINARY} binary." ozo-log
-        RETURN=1
-      fi
-    done
-    # check that mariaDB service is started
-    if ! systemctl is-active --quiet mariadb
+    # Function   : ozo-mariadb-dump-validate-configuration
+    # Description: Performs a series of checks against the script configuration. Returns 0 (TRUE) if all checks pass and 1 (FALSE) if any check fails.
+
+    # Control variable
+    local RETURN=0
+    # Determine if the configuration file exists
+    if [[ -f "${MARIADB_DUMP_CONFIGURATION}" ]]
     then
-      LEVEL="err" MESSAGE="MariaDB service is not running." ozo-log
-      RETURN=1
+        # Configuration file exists; source it
+        source "${MARIADB_DUMP_CONFIGURATION}"
+        # Iterate through all user-defined variables
+        for USERDEFVAR in MARIADB_DUMP_USER MARIADB_DUMP_PASS MARIADB_DUMP_DIR MARIADB_DUMP_SKIP_DB
+        do
+            # Determine if the variable is null
+            if [[ -z "${!USERDEFVAR}" ]]
+            then
+                # Variable is null
+                LEVEL="err" MESSAGE="User-defined variable ${USERDEFVAR} is not set." ozo-log
+                RETURN=1
+            fi
+        done
+        # Iterate through required binaries
+        for BINARY in mysql mysqldump mkdir chown find bzip2 rm
+        do
+            # Determine if binary does not exist
+            if ! which ${BINARY}
+            then
+                # Binary does not exist
+                LEVEL="err" MESSAGE="Missing ${BINARY} binary." ozo-log
+                RETURN=1
+            fi
+        done
+        # Determine if mariaDB service is not started
+        if ! systemctl is-active --quiet mariadb
+        then
+            # Service is not started
+            LEVEL="err" MESSAGE="MariaDB service is not running." ozo-log
+            RETURN=1
+        fi
+    else
+        # Configuration file does not exist
+        LEVEL="err" MESSAGE="Missing configuration file ${MARIADB_DUMP_CONFIGURATION}" ozo-log
+        RETURN=1
     fi
-  else
-    LEVEL="err" MESSAGE="Missing configuration file ${MARIADB_DUMP_CONFIGURATION}" ozo-log
-    RETURN=1
-  fi
-  if ozo-mariadb-dump-create-output-dir
-  then
-    chmod 0700 "${MARIADB_DUMP_DIR}"
-  else
-    RETURN=1
-  fi
-  if [[ ${RETURN} == 0 ]]
-  then
-    LEVEL="info" MESSAGE="Configuration validates." ozo-log
-  else
-    LEVEL="err" MESSAGE="Error validating configuration." ozo-log
-    RETURN=1
-  fi
-  return ${RETURN}
+    # Return
+    return ${RETURN}
 }
 
 function ozo-mariadb-dump-databases {
-  ### Generates a list of databases and performs a dump of each, skipping databases in MARIADB_DUMP_SKIP_DB
-  ### Returns 0 (TRUE) if all databases dump and 1 (FALSE) if any one job fails
-  local RETURN=0
-  DATABASES="$(mysql -u ${MARIADB_DUMP_USER} -p${MARIADB_DUMP_PASS} -Bse 'show databases')"
-  # check that databases were found
-  if [[ -n ${DATABASES} ]]
-  then
-    # databases found; loop though the list
-    for DATABASE in ${DATABASES}
-    do
-      # check if database is found in MARIADB_DUMP_SKIP_DB
-      if echo "${MARIADB_DUMP_SKIP_DB}" | grep -q "${DATABASE}"
-      then
-        # database is found in MARIADB_DUMP_SKIP_DB; skip it
-        LEVEL="warning" MESSAGE="Database ${DATABASE} found in MARIADB_DUMP_SKIP_DB; skipping." ozo-log
-      else
-        # database is not found in MARIADB_DUMP_SKIP_DB; dump it
-        MARIADB_DUMP_FILE="${MARIADB_DUMP_DIR}/$(date +%Y%m%d-%H%M%S)-${DATABASE}-mysqldump.sql"
-        if mysqldump -u ${MARIADB_DUMP_USER} -p${MARIADB_DUMP_PASS} ${DATABASE} --ignore-table=mysql.event --flush-logs --routines=TRUE --default-character-set=utf8 --skip-lock-tables --single-transaction > ${MARIADB_DUMP_FILE}
-        then
-          # database dumped; compress it
-          if bzip2 "${MARIADB_DUMP_FILE}"
-          then
-            rm -f "${MARIADB_DUMP_FILE}"
-            # remove dump files older than three days
-            find "${MARIADB_DUMP_DIR}/" -name "*-${DATABASE}-mysqldump*" -mtime +${MARIADB_DUMP_KEEP_DAYS} -exec rm {} \;
-            # log
-            LEVEL="info" MESSAGE="Successfully dumped database ${DATABASE}." ozo-log
-          else
-            LEVEL="err" MESSAGE="Error compressing ${MARIADB_DUMP_FILE}" ozo-log
-            RETURN=1
-          fi
-        else
-          # database did not dump; log
-          LEVEL="err" MESSAGE="Error dumping ${DATABASE}" ozo-log
-        fi
-      fi
-    done
-  else
-    # no databases found
-    LEVEL="warning" MESSAGE="No databases found." ozo-log
-    RETURN=1
-  fi
-  return ${RETURN}
+    # Function   : ozo-mariadb-dump-databases
+    # Description: Generates a list of databases and performs a dump of each, skipping databases in MARIADB_DUMP_SKIP_DB. Returns 0 (TRUE) if all databases dump and 1 (FALSE) if any one job fails.
+
+    # Control variable
+    local RETURN=0
+    # Obtain list of databases
+    DATABASES="$(mysql -u ${MARIADB_DUMP_USER} -p${MARIADB_DUMP_PASS} -Bse 'show databases')"
+    # Determine that DATABASES is not null
+    if [[ -n ${DATABASES} ]]
+    then
+        # DATABASES is not null; iterate through the databases
+        for DATABASE in ${DATABASES}
+        do
+            # Determine if database is found in MARIADB_DUMP_SKIP_DB
+            if echo "${MARIADB_DUMP_SKIP_DB}" | grep -q "${DATABASE}"
+            then
+                # Database is found in MARIADB_DUMP_SKIP_DB; skip it
+                LEVEL="warning" MESSAGE="Database ${DATABASE} found in MARIADB_DUMP_SKIP_DB; skipping." ozo-log
+            else
+                # Database is not found in MARIADB_DUMP_SKIP_DB; log
+                MARIADB_DUMP_FILE="${MARIADB_DUMP_DIR}/$(date +%Y%m%d-%H%M%S)-${DATABASE}-mysqldump.sql"
+                # Determine if database dump succeeded
+                if mysqldump -u ${MARIADB_DUMP_USER} -p${MARIADB_DUMP_PASS} ${DATABASE} --ignore-table=mysql.event --flush-logs --routines=TRUE --default-character-set=utf8 --skip-lock-tables --single-transaction > ${MARIADB_DUMP_FILE}
+                then
+                    # Success; Determine if compressing the dump file succeeded
+                    if bzip2 "${MARIADB_DUMP_FILE}"
+                    then
+                        # Success; remove the dump file
+                        rm -f "${MARIADB_DUMP_FILE}"
+                        # Remove dump files older than the configured number of days
+                        find "${MARIADB_DUMP_DIR}/" -name "*-${DATABASE}-mysqldump*" -mtime +${MARIADB_DUMP_KEEP_DAYS} -exec rm {} \;
+                        # Log
+                        LEVEL="info" MESSAGE="Successfully dumped database ${DATABASE}." ozo-log
+                    else
+                        # Failure
+                        LEVEL="err" MESSAGE="Error compressing ${MARIADB_DUMP_FILE}" ozo-log
+                        RETURN=1
+                    fi
+                else
+                    # Database did not dump; log
+                    LEVEL="err" MESSAGE="Error dumping ${DATABASE}" ozo-log
+                fi
+            fi
+        done
+    else
+        # No databases found
+        LEVEL="warning" MESSAGE="No databases found." ozo-log
+        RETURN=1
+    fi
+    # Return
+    return ${RETURN}
 }
 
 function ozo-mariadb-dump-program-loop {
-  ### Validates configuration, creates output dir, dumps databases, performs maintenance
-  ### Returns 0 (TRUE) if all methods succeed and 1 (FALSE) if any method fails
-  local RETURN=0
-  if ozo-mariadb-dump-validate-configuration
-  then
-    if ozo-mariadb-dump-create-output-dir
+    # Function   : ozo-mariadb-dump-program-loop
+    # Description: Validates configuration, creates output dir, dumps databases, performs maintenance. Returns 0 (TRUE) if all methods succeed and 1 (FALSE) if any method fails.
+
+    # Control variable
+    local RETURN=0
+    # Determine if configruation validates
+    if ozo-mariadb-dump-validate-configuration
     then
-      if ! ozo-mariadb-dump-databases
-      then
-        RETURN=1
-      fi
+        # Configuration validates; determine if output directory exists or has been created
+        if ozo-mariadb-dump-create-output-dir
+        then
+            # Output directory exists or has been created; determine if database dumping does not succeed
+            if ! ozo-mariadb-dump-databases
+            then
+                # Databases failed to dump
+                LEVEL="err" MESSAGE="Error dumping databases" ozo-log
+                RETURN=1
+            fi
+        else
+            # Output directory does not exist and could not be created
+            LEVEL="err" MESSAGE="Output directory not found and could not be created" ozo-log
+            RETURN=1
+        fi
     else
-      RETURN=1
+        # Configuration failed to validate
+        LEVEL="err" MESSAGE="Configuration did not validate" ozo-log
+        RETURN=1
     fi
-  else
-    RETURN=1
-  fi
-  return ${RETURN}
+    return ${RETURN}
 }
 
 # MAIN
-
+# Control variable
 EXIT=0
-
-LEVEL="info" MESSAGE="Starting MariaDB Backup." ozo-log
+# Set variables
+MARIADB_DUMP_CONFIGURATION="/etc/ozo-mariadb-backup.conf"
+# Log a process start message
+LEVEL="info" MESSAGE="OZO MariaDB Backup process starting." ozo-log
+# Determine if ozo-mariadb-backup succeeded
 if ozo-mariadb-dump-program-loop > /dev/null 2>&1
 then
-  LEVEL="info" MESSAGE="Finished MariaDB Backup with success." ozo-log
+    # Success
+    LEVEL="info" MESSAGE="Finished MariaDB Backup with success." ozo-log
 else
-  LEVEL="info" MESSAGE="Finished MariaDB Backup with errors." ozo-log
-  EXIT=1
+    # Failure
+    LEVEL="info" MESSAGE="Finished MariaDB Backup with errors." ozo-log
+    EXIT=1
 fi
-
+# Log a process complete message
+LEVEL="info" MESSAGE="OZO MariaDB Backup process complete." ozo-log
+# Exit
 exit ${EXIT}
